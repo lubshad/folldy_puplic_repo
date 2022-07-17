5952b115b43bfeccc6202abf54bab29e63fa03db
lubshad <lubshad4u4@gmail.com>
2022-07-17 21:12:39 +0530

    17

--- copyteacher.sh
+++ copyteacher.sh
@@ -1,3 +1,7 @@
-echo filename?
-read filename
-scp ../folldy_teacher/build/app/outputs/apk/release/app-release.apk $filename
+echo removedate?
+read removedate
+rm -r folldy_teacher_$removedate.apk
+echo date?
+read date
+scp ../folldy_teacher/build/app/outputs/apk/release/app-release.apk folldy_teacher_$date.apk
+
